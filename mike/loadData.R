7114df097773e6b5d7033f2567b3c2964724f366
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2021-11-18 13:17:49 -0400

    loads data to Global env and adds parameter to control output messages

--- mike/loadData.R
+++ mike/loadData.R
@@ -3,21 +3,25 @@
 #' environment.  The object names will be identical to the original file names.
 #' @param dataPath default is \code{NULL} the folder containing files to be loaded into R.  If left 
 #' as NULL, csv files in the\code{sampleAndesData} folder will be loaded
+#' @param quiet default is \code{FALSE} Determines whether or not the script should output 
+#' informational messages 
 #' @return nothing - just loads data to environment
 #' @family general_use
 #' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
 #' @export
-loadData <- function(dataPath = NULL){
+loadData <- function(dataPath = NULL, quiet = FALSE){
   if (is.null(dataPath)){
     setwd(here::here())
     dataPath="samplAndesData"
     message("Loading sample data")
   }
   filenames <- list.files(dataPath, pattern=".*csv")
+  if (length(filenames)<1)stop("No csv files found")
   for(i in 1:length(filenames))
   {
     thisFile = filenames[i]
     thisFileName <- sub('\\.csv$', '', thisFile) 
-    assign(thisFileName, read.csv(file.path(dataPath,thisFile)))
+    assign(thisFileName, read.csv(file.path(dataPath,thisFile)), envir = .GlobalEnv)
+    if (!quiet) message("loaded ", thisFile)
   }
 }
