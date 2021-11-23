bc60fa736957fee5314dd5567a95ce0dac10ffbd
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2021-11-23 12:13:21 +0000

    adds tabs parameter to allow selecction of subset of tables.  Adds checks to ensure selected tables are valid

--- R/exeExtractor.R
+++ R/exeExtractor.R
@@ -5,18 +5,29 @@
 #' \code{Mar.utils::make_oracle_cxn()}. 
 #' @param mission default is \code{NULL}.  This is a vector of one or more mission identifiers 
 #' (e.g "CAR2021240") that will be used to limit the extractions to particular Mission(s). 
+#' @param tabs default is \code{c("ESE_MISSIONS", "ESE_SETS", "ESE_CATCHES", "ESE_BASKETS", 
+#' "ESE_SPECIMENS", "ESE_LV1_OBSERVATIONS")}.  This is a vector the ESE tables that should be 
+#' extracted.  By default, all will be extracted, but fewer names can be sent to extract a subset.
 #' @param quiet default is \code{FALSE} Determines whether or not the script should output 
 #' informational messages 
 #' @return nothing - just loads data to environment
 #' @family general_use
 #' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
 #' @export
-eseExtractor <- function(cxnObj = NULL, mission = NULL, quiet = FALSE){
+eseExtractor <- function(cxnObj = NULL, mission = NULL, tabs = c("ESE_MISSIONS", "ESE_SETS", "ESE_CATCHES", "ESE_BASKETS", "ESE_SPECIMENS", "ESE_LV1_OBSERVATIONS"), quiet = FALSE){
   if (is.null(cxnObj))stop("Can't proceed without valid cxnObj")
-  tabs <- c("ESE_MISSIONS", "ESE_SETS", "ESE_CATCHES", "ESE_BASKETS", "ESE_SPECIMENS", "ESE_LV1_OBSERVATIONS")
-  for (t in 1:length(tabs)){
-    qry <- paste0("Select * from GROUNDFISH.",tabs[t]," WHERE MISSION IN (",Mar.utils::SQL_in(mission, apos=T),")")
-    assign(x = tabs[t], value = cxnObj$thecmd(cxnObj$channel, qry), envir = .GlobalEnv)
-    if (!quiet) message("Loaded ",tabs[t]," for mission(s) '",paste0(mission, collapse = "', '"), "' into the local environment")
+  tabs <- toupper(tabs)
+  #Verify that requested tables exist
+  tabsExist <- c("ESE_MISSIONS", "ESE_SETS", "ESE_CATCHES", "ESE_BASKETS", "ESE_SPECIMENS", "ESE_LV1_OBSERVATIONS") 
+  tabsValid <- intersect(tabs,tabsExist)
+  if (!quiet && length(tabsValid)<length(tabs)) message("The following requested tables either don't exist, or can't be extracted by this function:\n",paste0(setdiff(tabs,tabsExist), collapse=", "))
+  if (length(tabsValid)<1){
+    message("No valid tables requested, stoppping")
+    return(NULL)
+  }
+  for (t in 1:length(tabsValid)){
+    qry <- paste0("Select * from GROUNDFISH.",tabsValid[t]," WHERE MISSION IN (",Mar.utils::SQL_in(mission, apos=T),")")
+    assign(x = tabsValid[t], value = cxnObj$thecmd(cxnObj$channel, qry), envir = .GlobalEnv)
+    if (!quiet) message("Loaded ",tabsValid[t]," for mission(s) '",paste0(mission, collapse = "', '"), "' into the local environment")
   }
 }
