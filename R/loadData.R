7d8e0e351b72eeb94d7eed6d083c3e4e3ff4bfa4
pablov <pevweb@gmail.com>
2021-11-25 23:35:18 -0400

    tweaks to he Oracle load function

--- R/loadData.R
+++ R/loadData.R
@@ -9,7 +9,7 @@
 #' @family general_use
 #' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
 #' @export
-loadData <- function(dataPath = "inst/sampleData/", quietly = FALSE){
+loadData <- function(dataPath = .andesData$defaultCsvPath, quietly = FALSE){
   #add trailing "/" if necess
   if(substr(dataPath ,(nchar(dataPath)+1)-1,nchar(dataPath)) != "/")dataPath = paste0(dataPath,"/")
   
@@ -19,7 +19,7 @@ loadData <- function(dataPath = "inst/sampleData/", quietly = FALSE){
   {
     thisFile = filenames[i]
     thisFileName <- sub('\\.csv$', '', thisFile) 
-    assign(thisFileName, read.csv(file.path(dataPath,thisFile)), envir = .GlobalEnv)
-    if (!quietly) message("loaded ", dataPath,thisFile)
+    assign(thisFileName, read.csv(file.path(dataPath,thisFile), stringsAsFactors=FALSE), envir = .GlobalEnv)
+    if (!quiet) message("loaded ", dataPath,thisFile)
   }
 }
