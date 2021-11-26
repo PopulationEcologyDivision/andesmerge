15e587c9d37fea690d12f8d7d249867cfe9c966d
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2021-11-26 18:28:29 +0000

    improves integrityCheck

--- R/integrityCheck.R
+++ R/integrityCheck.R
@@ -2,36 +2,58 @@
 #' @description This function verifies that key fields within one ESE table exist in the other 
 #' tables in the ESE.  For example, if a SETNO exists in ESE_CATCHES that does not exist in ESE_SETS,
 #' a message would be output.
+#' @param cxnObj default is \code{NULL}.  This is a connection object from 
+#' \code{Mar.utils::make_oracle_cxn()}. 
+#' @param mission default is \code{NULL}.  This is a vector of one or more mission identifiers 
+#' (e.g "CAR2021240") that will be used to limit the extractions to particular mission(s). 
+#' \code{"ALL"} is also valid, and will return all records for all of the specified tables. 
+#' Use with caution.
+#' @param type1Only default is \code{FALSE}. The presence of failed tows can result in a cascade of 
+#' 'missing' records in other tables. Setting this to \code{TRUE} will output which sets are not 
+#' type 1, but will then drop them from all other tables.
+#' @param debug default is \code{FALSE}.  When TRUE, this captures the ESE output locally, allowing 
+#' multiple runs without having to re-extract the data
+#' @param addErrors default is \code{TRUE}.  When \code{TRUE}, this modifies some SETNO, SPEC and 
+#' SIZE_CLASS to review how such changes will be handles by the function.  It will only work if 
+#' \code{debug = T}
 #' @return nothing - just outputs messages
-#' @family general_use
+#' @family qc
 #' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
 #' @export
-integrityCheck <- function(cxnObj = NULL, mission = NULL, type1Only =T){
-  parentlessChildren <- function(parent = NULL, parentKey = NULL, child = NULL, childKey = NULL, keyField = NULL){
-    res <- setdiff(childKey,parentKey)
-    if (length(res)==0){
-      message("\tAll values of ",paste0(keyField, collapse=","), " that exist in ", child," can be found in ",parent)
-    }else{
-      message("\tPC The following combinations of ",paste0(keyField, collapse="_")," exist in ", child,", but NOT in ",parent)
-      paste0("\t\t",unique(res),collapse=", ")
-    }
-  }
+integrityCheck <- function(cxnObj = NULL, mission = NULL, type1Only =FALSE, debug =FALSE, addErrors=FALSE){
   setTypeCheck <- function(setTable=NULL){
-    res<- setTable[which(setTable$EXPERIMENT_TYPE_CODE != 1),c("MISSION", "SETNO")]
-    if(nrow(res)>0){
-      res <- paste0(res$MISSION, "_",res$SETNO)
-      message("ESE_SETS includes set(s) other than EXPERIMENT_TYPE_CODE  = 1. To avoid false positives, other tables will NOT report when these sets are missing") 
+    typeXSets<- setTable[which(is.na(setTable$EXPERIMENT_TYPE_CODE) | setTable$EXPERIMENT_TYPE_CODE != 1),]
+    typeXSets <- typeXSets[,c("MISSION", "SETNO", "START_DATE", "START_TIME", "END_DATE", "END_TIME", "STRAT", "SLAT", "SLONG", "ELAT", "ELONG", "DIST", "EXPERIMENT_TYPE_CODE", "GEAR","NOTE")]
+    if(nrow(typeXSets)>0){
+      res <- paste0(typeXSets$MISSION, "_",typeXSets$SETNO)
+      message("ESE_SETS includes the following set(s) with EXPERIMENT_TYPE_CODE  != 1. 
+To avoid false positives, other tables will NOT report when these sets are missing") 
+      print(typeXSets)
     }else{
       res <- NA
     }
     return(res)
   }
   
-  qcEnv <- new.env()
-  eseExtractor(cxnObj = cxnObj, mission=mission, env = qcEnv)
+  if (!exists("qcEnv", envir = .GlobalEnv)){
+    # if debug is True, don't re-extract ese if they are available.
+    qcEnv <- new.env()
+    eseExtractor(cxnObj = cxnObj, mission=mission, env = qcEnv)
+    if (debug) {
+      .GlobalEnv$qcEnv<-rlang::env_clone(qcEnv)
+      message("Saved copy of extracted ESE data for future debugging")
+    }
+  }else{
+    if (debug){
+      qcEnv<-rlang::env_clone(.GlobalEnv, env = qcEnv)
+      message("Loaded old copies of ESE data")
+    } 
+  }
+  
   toCheck <- getEseTables()
   
   if (type1Only){
+    #drop all cases where non-type1 sets are referenced in other tables
     notableSets <- setTypeCheck(setTable = qcEnv$ESE_SETS)
     for (i in 1:length(toCheck)){
       if (all(c("MISSION","SETNO") %in% names(qcEnv[[toCheck[i]]]))) {
@@ -39,28 +61,41 @@ integrityCheck <- function(cxnObj = NULL, mission = NULL, type1Only =T){
       }
     }
   }
-  
+  if (debug && addErrors){
+    #add errors
+    qcEnv$ESE_CATCHES[qcEnv$ESE_CATCHES$SETNO %in% c(15),"SETNO"]<- 115
+    qcEnv$ESE_BASKETS[qcEnv$ESE_BASKETS$SPEC %in% c(11),"SPEC"]<- 12345
+    qcEnv$ESE_SPECIMENS[qcEnv$ESE_SPECIMENS$SIZE_CLASS %in% c(2),"SIZE_CLASS"]<- 18
+  }
   for (p in 1:length(toCheck)){
     parentTabName <- toCheck[p]
     parentTabFields <- getKeyFields(parentTabName)
     parentTab <- qcEnv[[parentTabName]]
-    message("\t",parentTabName)
+    message(parentTabName)
     childTabNames <- setdiff(toCheck, parentTabName)
     for (c in 1:length(childTabNames)){
       childTabName <- childTabNames[c]
       childTabFields <- getKeyFields(childTabName)
       childTab <- qcEnv[[childTabName]]
-      theMessage <- paste0("\t\t", childTabName)
       compareFields <- intersect(childTabFields, parentTabFields)
-      
-      ttt<-dplyr::setdiff(parentTab[,compareFields],childTab[,compareFields])
+      if ((parentTabName == "ESE_SPECIMENS" & childTabName %in% c("ESE_CATCHES","ESE_BASKETS")) |
+          (parentTabName == "ESE_LV1_OBSERVATIONS" & childTabName %in% c("ESE_CATCHES","ESE_BASKETS"))
+      ){
+        message("\tvs ", childTabName,": (Skipping check on ",paste0(compareFields, collapse=","),")")
+        next
+      }
+      ttt<-dplyr::setdiff(childTab[,compareFields],parentTab[,compareFields])
       if(length(ttt)>0){
         f<-merge(parentTab, ttt, by=compareFields, all.y=T)
         if (nrow(f)>0){
-          message(theMessage)
-          print(head(f,20))
+          message("\tvs ", childTabName,": The following records can not be linked on ",paste0(compareFields, collapse=","),": ")
+          if (debug){
+            print(head(f,5))
+          }else{
+            print(f)
+          }
         }else{
-          message(theMessage, " - checked")
+          message("\tvs ", childTabName,": All records can be linked on ",paste0(compareFields, collapse=","))
         }
       }
     }
--- inst/mikeTesting/mikeTesting.R
+++ inst/mikeTesting/mikeTesting.R
@@ -1,15 +1,14 @@
 R.utils::sourceDirectory("c:/git/MMMcMahon/andesmerge/R/", modifiedOnly=F)
-setwd("c:/git/MMMcMahon/andesmerge/")
 # loadData()
-Mar.datawrangling::get_data_custom(schema = 'groundfish', tables = "ESE_MISSIONS", data.dir = data.dir, env = .GlobalEnv)
+# Mar.datawrangling::get_data_custom(schema = 'groundfish', tables = "ESE_MISSIONS", data.dir = data.dir, env = .GlobalEnv)
 
+oraCxn <- Mar.utils::make_oracle_cxn('rodbc', fn.oracle.dsn = "PTRAN_64")
 # library(andesmerge)
 eseExtractor(cxnObj = oraCxn, mission = "CAR2021240")
 #QC
-# tt<-sapply(X=getEseTables(), FUN=checkPrimaryFields)
+integrityCheck(cxnObj = oraCxn, mission = "CAR2021240")
 
 
-oraCxn <- Mar.utils::make_oracle_cxn('rodbc', fn.oracle.dsn = "PTRAN_64")
 # oraCxn2 <- Mar.utils::make_oracle_cxn('roracle', fn.oracle.dsn = "PTRAN")
 
 toOracle(cxnObj = oraCxn, source_df = ESE_BASKETS, target_table = "ESE_BASKETS", target_schema = "mcmahonm", createReplaceTarget = F)
