fb79d94840a438dbf00b69b9544d5e4d6fb48ba9
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2022-04-21 11:50:54 +0000

    make new experiment_type_id_tweaked to hold correct experiment_type_id values, and handle hydrography sets, which have set.res == NA

--- R/setExperimentType.R
+++ R/setExperimentType.R
@@ -12,7 +12,8 @@
 setExperimentType <- function(x, quiet = FALSE){
   
   
-  exp.num =  x$experiment_type_id
+  #exp.num =  x$experiment_type_id
+  exp.num =  as.numeric(gsub("(^[0-9]{1,2})(.*$)", "\\1", x$experiment_type))
   set.res =  x$set_result_id
   
   
@@ -24,7 +25,9 @@ setExperimentType <- function(x, quiet = FALSE){
   #   7	Gear testing		
   #   99	Systematic		
   valid.exp.num = c(1, 5, 6, 7, 9, 99)
-  if(!all(exp.num %in% valid.exp.num)) stop("Expedition numbers not in list")
+  if(!all(exp.num %in% valid.exp.num)) stop("Unknown Experiment type(s) found")
+  x$experiment_type_id_tweaked <- NA
+  x$experiment_type_id_tweaked <- as.numeric(gsub("(^[0-9]{1,2})(.*$)", "\\1", x$experiment_type))
   
   # Possible set result values (from  : andesdb.shared_models_setresult)
   # 1	NORMAL - No damage to gear	
@@ -33,24 +36,34 @@ setExperimentType <- function(x, quiet = FALSE){
   # 4	FAILED - Bad depth
   # 5	FAILED - Fishing gears
   # 6	FAILED - Wrong gear operation
-  valid.result.num = c(1, 2, 3, 4, 5, 6)
+  # MMM handle hydrography - exp.num = 9 and is.na(set.res)
+  # NA Normal - only if exp.num ==9 - Hydrography
+  valid.result.num = c(1, 2, 3, 4, 5, 6, NA)
   if(!all(set.res %in% valid.result.num)) stop("Set result not in list")
   
   #NORMAL - GOOD
   index = (exp.num %in% valid.exp.num & set.res %in% c(1, 2))
   if(length(which(index)) > 0)
   {
-    x[index,]$experiment_type = 1
+    x[index,]$experiment_type_id_tweaked = 1
     if(!quiet){ message("Experiment type set to 1")}
   }
   
   #NORMAL - FAIL
   index = (exp.num %in% valid.exp.num & set.res %in% c(3, 4, 5, 6))
   if(length(which(index)) > 0 ){
-    x[index,]$experiment_type = 3
+    x[index,]$experiment_type_id_tweaked = 3
     if(!quiet){ message("Experiment type set to 3")}
   }
   
+  #NORMAL - HYDROGRAPHY
+  #Added by MMM - not sure about using an NA as an identifier, but maybe in combo with exp.num==9 
+  index = (exp.num == 9 & is.na(set.res))
+  if(length(which(index)) > 0 ){
+    x[index,]$experiment_type_id_tweaked = 9
+    if(!quiet){ message("Experiment type set to 9")}
+  }
   
-  return(as.numeric(x$experiment_type))
+  
+  return(as.numeric(x$experiment_type_id_tweaked))
 }
