eaa210ff93f105038f192b871e2c5f746c3e48cf
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2022-04-28 19:16:05 +0000

    adds global variables

--- R/zzz.R
+++ R/zzz.R
@@ -1,2 +1,3 @@
 utils::globalVariables(c("tmp_set_data", "tmp_catch_data","tmp_basket_data","tmp_obs_types_data",
-                         "tmp_specimen_data","tmp_cruise_data","ESE_SETS","value", "variable"))
+                         "tmp_specimen_data","tmp_cruise_data","ESE_SETS","value", "variable",
+                         "SETNO", "SPEC", "SIZE_CLASS", "SPECIMEN_ID", "LV1_OBSERVATION"))
