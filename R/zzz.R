56da1bf383c53382ccea17f78844f4d3029ebdba
MMMcMahon <Mike.McMahon@dfo-mpo.gc.ca>
2023-02-06 19:19:12 +0000

    fixes

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,4 @@
 utils::globalVariables(c("tmp_set_data", "tmp_catch_data","tmp_basket_data","tmp_obs_types_data",
                          "tmp_specimen_data","tmp_cruise_data","ESE_SETS","value", "variable",
-                         "MISSION", "SETNO", "SPEC", "SIZE_CLASS", "SPECIMEN_ID", "LV1_OBSERVATION"))
+                         "MISSION", "SETNO", "SPEC", "SIZE_CLASS", "SPECIMEN_ID", "LV1_OBSERVATION",
+                         "SPRING_4VW", "SPRING_4X", "SUMMER_4VWX", "GEORGES_5Z"))
